ffecefcb8edddff0b36d543d10a6e7e7d5b239c4
XJIEBAA <matveyreut722@gmail.com>
2024-12-21 13:20:32 +0300

    init(sync)

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom'
+
+const App = () => {
+    return <div>Hello World</div>
+}
+
+export default App
