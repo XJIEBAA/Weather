06e6b01bbe94a3e0056d54f6b99204c4ee51a679
XJIEBAA <matveyreut722@gmail.com>
2024-12-22 15:10:40 +0300

    sync(project): get check function for weather component

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, { use, useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import object from "../../store/counterSlice.js"
 
-import { container, title, pretitle, days } from "../css/style.css"
+import { container, containerPadding, title, pretitle, days } from "../css/style.css"
 
 import Day from "./Day.js"
 
@@ -26,19 +26,20 @@ const App = () => {
 
     return (
         <div className={container}>
-            <div className={title}>
-                {current.length > 0 ? current[0].day.condition.text : null}
+            <div className={containerPadding}>
+                <div className={title}>
+                    {current.length > 0 ? current[0].day.condition.text : null}
+                </div>
+                <div className={pretitle}>
+                    {location.length > 0 ? location[0].tz_id : null}
+                </div>
             </div>
-            <div className={pretitle}>
-                {location.length > 0 ? location[0].tz_id : null}
-            </div>
+            
             <div class={days}>
                 {weather.length > 0 ? weather[0].forecast.forecastday.map((item, key) => {
                     console.log(key)
                     return <Day item={item} id={key}></Day>
                 }) : null}
-            </div>
-            <div>
             </div>
         </div>
     )
--- src/components/Day.js
+++ src/components/Day.js
@@ -2,19 +2,42 @@ import React, { use, useEffect } from "react"
 
 import { useSelector, useDispatch } from "react-redux"
 import object from "../../store/counterSlice.js"
-import { day } from "../css/style.css"
+import { wDay } from "../css/style.css"
 
-import { Cloudy } from "../public/Cloudy.svg"
-import { MostlyCloudyNight } from "../public/MostlyCloudy-night.svg"
-import { MostlyCloudy } from "../public/MostlyCloudy.svg"
+import Cloudy from "../public/Cloudy.svg"
+import MostlyCloudyNight from "../public/MostlyCloudy-night.svg"
+import MostlyCloudy from "../public/MostlyCloudy.svg"
+
+function getDays(day, days) {
+    let slice1 = days.slice(0, day)
+    let slice2 = days.slice(day, days.length)
+
+    return [...slice2, ...slice1]
+}
+
+function check(title, time) {
+    if (title == "Patchy rain nearby" && time > 6 && time < 23) {
+        return [MostlyCloudyNight, "Mostly Cloudy"]
+    } else if (title == "Patchy rain nearby" && time < 6 && time > 23) {
+        return [MostlyCloudyNight, "Mostly Cloudy"]
+    } else if ((title == "Mist" || title == "Cloudy") && time < 6 && time > 23) {
+        return [Cloudy, "Cloudy"]
+    }
+}
 
 function Day({ id, item }) {
     const weather = useSelector(state => state.weather)
     const dispatch = useDispatch()
 
+    let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    let date = new Date()
+    let day = date.getDay() + 6 
+
+    let newDays = getDays(day, days)
+
     return (
-        <div stlye={day} onClick={() => dispatch(object.actions.setCurrent({key: id, weather: weather}))}>
-            
+        <div className={wDay} onClick={() => dispatch(object.actions.setCurrent({key: id, weather: weather}))}>
+            {newDays[id]}
         </div>
     )
 }
